48124ad040ad67b1bf944688998573d81f46572f
Nico Mesina <nick.mesina@yahoo.com>
2022-07-17 10:30:43 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+function runGame() {
+    }
+
+
+    function checkAnswers() {
+
+    }
+
+    function calculateCorrectAnswers() {
+
+    }
+
+    function incrementWrongAnswers() {
+
+    }
+
+    function displayAdditionQuestion() {
+
+    }
+
+    function displaySubtractQuestion() {
+
+    }
+
+    function displayMultiplyQuestion() {
+        
+    }
