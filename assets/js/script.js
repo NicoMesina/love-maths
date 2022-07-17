b15ae16f8108dd152473ef8f251e4a3996e7c104
Nico Mesina <nick.mesina@yahoo.com>
2022-07-17 17:38:35 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,12 +44,14 @@ function incrementScore() {
 }
 
 function incrementWrongAnswer() {
+     }
 
-}
+    function displayAdditionQuestion(operand1, operand2) {
 
-function displayAdditionQuestion(operand1, operand2) {
-
-}
+        document.getElementById('operand1').textContent = operand1;
+        document.getElementById('operand2').textContent = operand2;
+        document.getElementById('operator').textContent = "+";
+        }
 
 function displaySubtractQuestion() {
 
